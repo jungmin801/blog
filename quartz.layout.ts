9ef26acff1d43ade69e6efc709ddfe9c3650282f
문정민 <130200440+jungmin801@users.noreply.github.com>
2025-05-21 13:48:40 +0900

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: "giscus",
+      options: {
+        // from data-repo
+        repo: "jungmin801/blog",
+        // from data-repo-id
+        repoId: "R_kgDOOsaoWQ",
+        // from data-category
+        category: "Announcements",
+        // from data-category-id
+        categoryId: "DIC_kwDOOsaoWc4CqZwg",
+      },
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jungmin801",
