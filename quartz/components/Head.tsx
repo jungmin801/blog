d73b49599b5d6b20a903f94e3c13853b316880c0
문정민 <130200440+jungmin801@users.noreply.github.com>
2025-05-27 22:05:28 +0900

    Update Head.tsx

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -37,7 +37,7 @@ export default (() => {
     const ogImageDefaultPath = `https://${cfg.baseUrl}/static/og-image.png`
 
     return (
-      <head>ß
+      <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
